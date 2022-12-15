70d34a620f4050faa42674d9583b095d1c1cbe00
theRobMay <robbie.maysent@gmail.com>
2022-12-15 14:19:50 -0600

    adding auth portion to react app

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,45 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router'
-import cookie from 'cookie'
-import Home from './components/Home'
-import About from './components/About'
-import Car from './components/Car'
-import Login from './components/Login'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router';
+import cookie from 'cookie';
+import Home from './components/Home';
+import About from './components/About';
+import Car from './components/Car';
+import Login from './components/Login';
 
-// Write checkAuth function here
-// Check the cookies for a cookie called "loggedIn"
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie);
+    // * Checking cookie object to see if cookies.loggedIn is truthy.
+    return cookies['loggedIn'] ? true : false;
+};
 
-
-// Write ProtectedRoute function here
-
+const ProtectedRoute = (props) => {
+    // # Destructure the props object.
+    // # Using the rest pattern to add the rest of the props assigned when we call the component.
+    const { component: Component, ...rest } = props;
+    console.log(rest);
+    // # Using Component variable assigned above to render a component if logged in.
+    // # If user is not logged in they are pointed back to the login page using the "Navigate" component from react-router.
+    // # Also unpacking the rest of the props into the component by using the spread operator.
+    return checkAuth() === true ? <Component /> : <Navigate to='/login' />;
+};
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/car/:id" element={<Car/>} />
+            {/* Setting component prop to Home for protected home route */}
+            <Route
+                path='/'
+                element={
+                    <ProtectedRoute
+                        component={Home}
+                        isTacoGood='true'
+                        size='Large Please'
+                    />
+                }
+            />
+            <Route path='/login' element={<Login />} />
+            <Route path='/about' element={<ProtectedRoute component={About} />} />
+            <Route path='/car/:id' element={<ProtectedRoute component={Car} />} />
         </Routes>
     );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { TextField, Button, Container } from "@mui/material";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TextField, Button, Container } from '@mui/material';
 
 const Login = () => {
   const navigate = useNavigate();
 
   const [state, setState] = useState({
-    username: "",
-    password: "",
+    username: '',
+    password: '',
   });
 
   const handleTextChange = (e) => {
@@ -22,43 +22,47 @@ const Login = () => {
 
   const login = (e) => {
     e.preventDefault();
-    // set cookie here
-    // set loggedIn = true and max-age = 60*1000 (one minute)
+    // # Send POST request to server for user.
 
-    navigate("/");
+    // # If successful...
+    //Todo: set cookie here
+    //Todo: set loggedIn = true and max-age = 60*1000 (one minute)
+    document.cookie = 'loggedIn=true;max-age=60*1000';
+    navigate('/');
   };
 
   return (
-    <div className="App">
-      <Container maxWidth="sm">
-        <form className="login-form" onSubmit={login}>
-          <TextField
-            required
-            onChange={handleTextChange}
-            value={state.username}
-            name="username"
-            label="Username"
-            type="text"
-          />
-          <TextField
-            required
-            onChange={handleTextChange}
-            value={state.password}
-            name="password"
-            label="Password"
-            type="password"
-          />
-          <Button
-            type="submit"
-            className="login-button"
-            variant="contained"
-            color="primary"
-          >
-            Login
-          </Button>
-        </form>
-      </Container>
-    </div>
+      <div className='App'>
+        <Container maxWidth='sm'>
+          <form className='login-form' onSubmit={login}>
+            <TextField
+                required
+                onChange={handleTextChange}
+                value={state.username}
+                name='username'
+                label='Username'
+                type='text'
+            />
+            <TextField
+                required
+                onChange={handleTextChange}
+                value={state.password}
+                name='password'
+                label='Password'
+                type='password'
+            />
+            <Button
+                onClick={Login}
+                type='submit'
+                className='login-button'
+                variant='contained'
+                color='primary'
+            >
+              Login
+            </Button>
+          </form>
+        </Container>
+      </div>
   );
 };
 
